b8b43c542a3f116969c99fcfe6f75cf35fe57e0c
Vítor Daniel Pontes Guedes <vitordaniel97.vtd@gmail.com>
2023-12-04 08:52:16 -0300

    refact: nenhuma alteração na lógica ou na aparência, somente padronização de código

--- src/js/main.js
+++ src/js/main.js
@@ -1,28 +1,28 @@
-const popup = document.getElementById("popup")
-const iframe = document.getElementById("iframe-principal")
-const botaoFecharPopup = document.getElementById("botao-fechar-popup")
-const quadrosContainer = document.getElementById("quadros-container")
-const quadros = document.querySelectorAll(".quadro")
+const popup = document.getElementById("popup");
+const iframe = document.getElementById("iframe-principal");
+const botaoFecharPopup = document.getElementById("botao-fechar-popup");
+const quadrosContainer = document.getElementById("quadros-container");
+const quadros = document.querySelectorAll(".quadro");
 
 quadros.forEach((quadro) => quadro.addEventListener("click", () => {
     if (!quadro.classList.contains("inativo")) {
         if (quadro.id !== "quadro-resultados") {
-            popup.style.display = "flex"
-            document.querySelector("#header-principal").classList.add("desfocado")
-            document.querySelector("#main-container").classList.add("desfocado")
-            document.querySelector("#footer-principal").classList.add("desfocado")
+            popup.style.display = "flex";
+            document.querySelector("#header-principal").classList.add("desfocado");
+            document.querySelector("#main-container").classList.add("desfocado");
+            document.querySelector("#footer-principal").classList.add("desfocado");
         }
     }
-}))
+}));
 
 function limparPopup() {
-    iframe.src = ""
+    iframe.src = "";
 }
 
 function fecharPopup() {
-    limparPopup()
-    popup.style.display = "none"
-    document.querySelector("#header-principal").classList.remove("desfocado")
-    document.querySelector("main").classList.remove("desfocado")
-    document.querySelector("#footer-principal").classList.remove("desfocado")
+    limparPopup();
+    popup.style.display = "none";
+    document.querySelector("#header-principal").classList.remove("desfocado");
+    document.querySelector("main").classList.remove("desfocado");
+    document.querySelector("#footer-principal").classList.remove("desfocado");
 }
