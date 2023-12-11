a7bfba57c491395857bc0dc207e9942e9328bab6
Vítor Daniel Pontes Guedes <vitordaniel97.vtd@gmail.com>
2023-12-11 16:31:53 -0300

    add: alguns preparos já inseridos no sistema

--- js/preparos.js
+++ js/preparos.js
@@ -1,3 +1,25 @@
+const imagemPreparo = document.querySelector("#imagem-preparo");
+const selectPreparo = document.querySelector("#select-preparo");
+// const radiosPeriodo = document.getElementsByName("periodo");
+const path = "../img/preparos"
+
 function exibePreparo() {
-    // Adicionar códigos para atualizar o preparo a ser exibido.
+    /*  Faltam preparos para
+        utilizar a função da
+        forma correta.
+     */
+
+    // let periodoSelected = Array.from(radiosPeriodo).find(r => r.checked).value;
+    
+    if (selectPreparo.value !== "0") {
+        imagemPreparo.classList = "imagem-preparo";
+        imagemPreparo.src = `${path}/${selectPreparo.value}.jpg`;
+    }
+    else {
+        imagemPreparo.classList = "elemento-oculto";
+    }
+
+
 }
+
+exibePreparo();
