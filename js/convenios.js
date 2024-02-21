26ac826769d7cd24157b5b8a7273d51dd7145980
Vítor Daniel Pontes Guedes <vitordaniel97.vtd@gmail.com>
2024-02-21 15:03:54 -0300

    add: tabela pacotes para convênios de saúde adicionada

---
+++ js/convenios.js
@@ -0,0 +1,60 @@
+const tabela = document.querySelector("#corpo-tabela");
+let convenios = [];  // novos convênios serão armazenados aqui
+let dados;
+const pacotes = ["mamografia", "ampliação", "densitometria", "us mamas"];
+
+
+fetch("https://vitordanielp.github.io/escalas/dados/pacotes/pacotes.json")
+    .then(response => response.json())
+    .then(response => {
+        dados = response;
+        getConvenios();
+        for (let convenio of convenios) {
+            addCells(convenio);
+        }
+    });
+
+
+class Convenio {
+    constructor(nome, pacotes) {
+        this.nome = nome;
+        this.pacotes = pacotes;
+    }
+}
+
+/*
+Cria nova instância de Convenio
+e armazena na Array convenios
+*/
+function addConvenio(nome, pacotes) {
+    let convenio = new Convenio(nome, pacotes);
+    convenios.push(convenio);
+}
+
+/*
+Insere os convênios na lista
+indicada no início do código.
+*/
+function getConvenios() {
+    for (let obj of dados) {
+        let pacotes = Object.values(obj);
+        let nome = pacotes.shift();
+        addConvenio(nome, pacotes);
+    }
+}
+
+/* 
+Adiciona os dados de cada
+convênio na tabela.
+*/
+function addCells(convenio) {
+    let content = `
+    <tr class="linha-convenio" id="${convenio.nome.replaceAll(" ", "-")}">
+    <th class="nome-convenio">${convenio.nome}`
+
+    tabela.innerHTML += content;
+    let linhaAtual = document.querySelector(`#${convenio.nome.replaceAll(" ", "-")}`);
+    for (let i in pacotes) {
+        linhaAtual.innerHTML += `<td class="${pacotes[i].replaceAll(" ", "-")}">${convenio.pacotes[i]}</td>`;
+    }
+}
