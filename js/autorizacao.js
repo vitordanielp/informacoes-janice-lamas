94d9c40fcc78d799fee057c8d540d607854c783a
Vítor Daniel Pontes Guedes <vitordaniel97.vtd@gmail.com>
2024-05-23 09:20:02 -0300

    add: convênio GDF adicionado para a autorizadora andressa

--- js/autorizacao.js
+++ js/autorizacao.js
@@ -27,7 +27,7 @@ function incluirAutorizador(nome, convenios) {
 incluirAutorizador(nome = "gisele sandra", convenios = ["AMHP", "CAESAN", "E-VIDA", "MPU", "SIS-SENADO", "TRE", "TRF", "TST"]);
 incluirAutorizador(nome = "kessia", convenios = ["AMIL", "ASSEFAZ", "BRADESCO", "BRB", "CAPESESP", "CONAB", "GAMA SAÚDE", "MEDSERVICE"]);
 incluirAutorizador(nome = "ana ribeiro", convenios = ["AFFEGO", "BACEN", "PETROBRAS", "SERPRO", "STJ", "STM", "TJDFT"]);
-incluirAutorizador(nome = "andressa", convenios = ["CAIXA", "CASEC", "CASEMBRAPA", "CASSI", "FASCAL", "STF", "TRT"]);
+incluirAutorizador(nome = "andressa", convenios = ["CAIXA", "CASEC", "CASEMBRAPA", "CASSI", "GDF", "FASCAL", "STF", "TRT"]);
 incluirAutorizador(nome = "raquel almeida", convenios = ["CÂMARA", "GEAP", "POLÍCIA FEDERAL", "POSTAL SAÚDE", "PROASA", "SUL AMÉRICA", "UNAFISCO", "UNIMED"]);
 incluirAutorizador(nome = "patricia", convenios = ["GESTÃO DE AGENDAS", "ACOMPANHAMENTO DE RESSONÂNCIA"]);
 
